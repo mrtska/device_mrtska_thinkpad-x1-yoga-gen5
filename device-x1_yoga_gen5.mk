bd197f44d28aa92257dde19ce38f754a332e124f
mrtska <mrtska.starring@gmail.com>
2021-01-01 09:17:20 +0900

    Added Bluetooth capability.

--- device-x1_yoga_gen5.mk
+++ device-x1_yoga_gen5.mk
@@ -173,9 +173,13 @@ PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
 # Bluetooth HAL
-#PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.1-service \
-    android.hardware.bluetooth@1.0-impl
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.1-service.btlinux \
+    android.hardware.bluetooth.audio@2.0-impl \
+    audio.bluetooth.default \
+    audio.a2dp.default
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
 # Wifi HAL
 PRODUCT_PACKAGES += \
